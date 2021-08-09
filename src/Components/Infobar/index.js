86f34db315400860fad480bbdb59c8740d07f7db
anishchaudhary27 <anishchaudhary2706@gmail.com>
2021-08-09 14:31:31 +0530

    link copy problem solved

--- src/Components/Infobar/index.js
+++ src/Components/Infobar/index.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import styles from './styles.module.css'
 import { useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react/cjs/react.production.min'
 
 export default function Infobar({ gameId, house, oponent }) {
     const history = useHistory()
+    const [link, setlink] = useState("")
     const handleLeave = () => {
         history.push('/')
     }
-    const copyInviteLink = () => {
+    useEffect(() => {
         if (house === 'w') {
-            navigator.clipboard.writeText(`http://${window.location.host}/?action=join&gameId=${gameId}&house=b`)
+            setlink(`http://${window.location.host}/?action=join&gameId=${gameId}&house=b`)
         }
         else {
-            navigator.clipboard.writeText(`http://${window.location.host}/?action=join&gameId=${gameId}&house=w`)
+            setlink(`http://${window.location.host}/?action=join&gameId=${gameId}&house=w`)
         }
+    }, [])
+    const copyInviteLink = () => {
+        navigator.clipboard.writeText(link)
     }
     return (
         <div className={styles.root}>
             <h2 style={{ color: "white" }}>Oponent</h2>
             <p className={styles.oponent}>{oponent}</p>
+            {oponent === '' && <p className={styles.oponent}>{link}</p>}
             {
                 oponent === '' &&
                 <button className={styles.invite} onClick={copyInviteLink}>
